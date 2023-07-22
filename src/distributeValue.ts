f0a0f3a2ab2dff1140bf07c971c705157a8427f9
Jeroen Bakker <jeroen.bakker@yoursurprise.com>
2023-07-22 10:59:41 +0200

    Swap position of parameters
    It makes sense for the value to be the most prominent parameter, so it should go first

--- src/distributeValue.ts
+++ src/distributeValue.ts
@@ -3,7 +3,7 @@ import { WeightedInteraction } from '../types';
 /**
  * Distributes a value over a list of weighted interactions, will overwrite previous values.
  */
-export default function distributeValue(interactions: WeightedInteraction[], value: number): Array<WeightedInteraction & {value: number}> {
+export default function distributeValue(value: number, interactions: WeightedInteraction[]): Array<WeightedInteraction & {value: number}> {
     return interactions.map((interaction: WeightedInteraction) => {
         return {
             ...interaction,
